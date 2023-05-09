8614bd542c4751777dfe3865fb7a5a9b18e6ec29
AleksKostadinov <kostadinov12@gmail.com>
2023-05-09 13:09:41 +0300

    Listing completed Todos

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import TodoSerializer
+from todo.models import Todo
 
-# Create your views here.
+
+class TodoCompletedList(generics.ListAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user, datecompleted__isnull=False).order_by('-datecompleted')
